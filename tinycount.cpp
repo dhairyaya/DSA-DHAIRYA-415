435df5477e2959735d66978ed250354f336cbad1
Dhairya Pratap Solanki <pratapdhairya67@gmail.com>
2026-09-09 09:35:03 +0530

    tiny count

---
+++ tinycount.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+    }
+   for(int i=0;i<n;i++){
+      int cnt=0;
+    for(int j=i+1;j<n;j++){
+        if(a[i]>a[j])  cnt++;
+    }
+    cout << cnt << " ";
+   }
+}
