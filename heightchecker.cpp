1437d0788db66892a72ff6ec7103b78fbcba68e6
Dhairya Pratap Solanki <pratapdhairya67@gmail.com>
2026-09-10 12:35:55 +0530

    height checker

---
+++ heightchecker.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+    }
+    vector<int> b=a;
+    sort(a.begin(),a.end());
+    int cnt=0;
+    for(int i=0;i<n;i++){
+        if(a[i]!=b[i]) cnt++;
+    }
+    cout << cnt << endl;
+}
